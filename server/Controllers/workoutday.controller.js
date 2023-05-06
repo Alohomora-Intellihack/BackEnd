e2135d8f1e63501c58082ceb05bc18c9dddbf407
Hana Jiffry <hanajiffryscg@gmail.com>
2023-05-06 20:05:42 +0530

    totCalories per day API

--- server/Controllers/workoutday.controller.js
+++ server/Controllers/workoutday.controller.js
@@ -31,19 +31,20 @@ const getExerciseForDay = async (req, res) => {
 //     });
 //   };
 
-//get total calories burnt for a week
-// const getCaloriesPerDay = async (req, res) => {
-//   const sqlQuery =
-//   "SELECT * FROM request r INNER JOIN site s ON r.SiteId  = s.siteid JOIN subscription s2 ON s.idSubscription  = s2.id WHERE r.idRequest=?";
-//   pool.query(sqlQuery,req.params.id, (err, rows, fields) => {
-//     if (!err) {
-//       console.log(rows);
-//       res.status(200).json(rows);
-//     } else {
-//       console.log(err);
-//       res.status(400).send(err.message);
-//     }
-//   });
-// };
+//get total calories burnt for a day
+const getCaloriesPerDay = async (req, res) => {
+  const day = req.params.day;
+  const sqlQuery = `SELECT SUM(calories_burned) AS totCalories FROM ${day}`
 
-module.exports = { getExerciseForDay };
+  pool.query(sqlQuery,req.params.id, (err, rows, fields) => {
+    if (!err) {
+      console.log(rows);
+      res.status(200).json(rows);
+    } else {
+      console.log(err);
+      res.status(400).send(err.message);
+    }
+  });
+};
+
+module.exports = { getExerciseForDay,getCaloriesPerDay };
--- server/Routes/workoutday.route.js
+++ server/Routes/workoutday.route.js
@@ -3,6 +3,6 @@ const controller = require('../Controllers/workoutday.controller');
 
 // workoutrouter.get('/',controller.getTotalCalories);
 workoutrouter.get('/exercises/:day',controller.getExerciseForDay);
-// workoutrouter.get('/totcalories',controller.getCaloriesPerDay)
+workoutrouter.get('/totcalories/:day',controller.getCaloriesPerDay)
 
 module.exports = workoutrouter;
